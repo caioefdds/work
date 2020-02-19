9a23f5262a19bd498b4387df9a3fe421ac7874e9
Caio Fagundes <caioefdds@gmail.com>
2020-02-19 19:56:37 -0300

    First commit: Portifolio base

---
+++ Portifolio/header.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+<link rel="stylesheet" href="bootstrap/css/bootstrap-grid-min.css" type="text/css">
+<link rel="stylesheet" href="css/class.basica.css" type="text/css">
+<script src="https://kit.fontawesome.com/c0e26ddfae.js" crossorigin="anonymous"></script>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script scr="bootstrap/js/bootstrap.min.js"></script>
+<script src="js/jquery-3.4.1.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
---
+++ Portifolio/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Caio Fagundes </title>
+</head>
+<?php include 'header.php'; ?>
+<body>
+
+<!-- Navigation bar -->
+<nav class="navbar navbar-expand-md navbar-dark azul-escuro">
+  <a class="navbar-brand titulo" href="#">PORTIFOLIO</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link vertical-spacer" href="#"><i class="fas fa-code"></i> My Projects</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link vertical-spacer" href="#"><i class="fas fa-pencil-alt"></i> About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link vertical-spacer" href="#"><i class="fas fa-address-card"></i> Contact</a>
+      </li>    
+    </ul>
+  </div>  
+</nav>
+
+<div class="container-fluid" style="background-color: blue;">
+  Primeira sessão
+</div>
+
+</body>
+</html>
