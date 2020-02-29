ad5df92fd202b9d29ac037ab274640a360d5df3d
Caio Fagundes <caioefdds@gmail.com>
2020-02-29 16:29:41 -0300

    Change: Links of header.php

--- Portifolio/header.php
+++ Portifolio/header.php
@@ -2,9 +2,9 @@
 <link rel="stylesheet" href="bootstrap/css/bootstrap-grid-min.css" type="text/css">
 <link rel="stylesheet" href="css/class.basica.css" type="text/css">
 <script src="https://kit.fontawesome.com/c0e26ddfae.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<script src="js/jquery.mask.min.js"></script>
 <script src="js/jquery-3.4.1.min.js"></script>
 <script scr="bootstrap/js/bootstrap.min.js"></script>
 <script src="js/class.basica.js"></script>
