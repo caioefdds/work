a0c350a5557a04b0f0eab17c14b9cd94f5e9cccb
Caio Fagundes <caioefdds@gmail.com>
2020-05-19 15:49:49 -0300

    Site do Nubank

---
+++ Nubank/index.php
@@ -0,0 +1,258 @@
+<?php include 'header.php'; ?>
+<html>
+<head>
+    <title>Nubank - Finalmente você no controle do seu dinheiro</title>
+    <link rel="shortcut icon" href="img/favicon.ico" />
+</head>
+<body>
+
+
+    <section id="navigation-bar">
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+            <a class="icone-nav" href=""><svg alt="Logomarca do Nubank" width="48" height="36" viewBox="0 0 48 36" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="#8A05BE" fill-rule="evenodd" clip-rule="evenodd" d="M46.372 5.69458H41.9424C41.0522 5.69458 40.3219 6.41986 40.3143 7.31081H40.3159L40.3123 7.4194L40.3143 7.50324L40.2945 7.77497H40.3148C40.3169 10.414 40.3153 23.5863 40.3143 28.8617H36.5418C34.4639 28.8617 32.7739 27.1804 32.7739 25.1131V7.31384C32.7739 6.42087 32.0432 5.69458 31.1454 5.69458H26.7122C25.8144 5.69458 25.0837 6.42087 25.0837 7.31384C25.0826 15.0904 25.0867 20.1038 25.0887 23.2418L25.0908 26.4399C25.0908 32.3629 31.7745 35.9999 36.5662 35.9999C39.7527 35.9999 41.7321 34.4564 41.7321 31.9715L41.7326 30.2724H43.1738C45.4742 30.2724 48 27.7506 48 25.454V7.31384C48 6.42087 47.2698 5.69458 46.372 5.69458ZM41.7321 7.31233C41.7331 7.19818 41.8276 7.10525 41.9424 7.10525H46.372C46.4877 7.10525 46.5817 7.19919 46.5817 7.31384V25.454C46.5817 26.9824 44.7028 28.8617 43.1738 28.8617H41.7326C41.7342 17.6986 41.7347 7.41688 41.7321 7.31233ZM36.5662 34.5893C32.3671 34.5893 26.5091 31.4886 26.5091 26.4399L26.507 23.4661C26.505 20.3447 26.5009 15.2783 26.502 7.31384C26.502 7.19919 26.5964 7.10525 26.7122 7.10525H31.1454C31.2611 7.10525 31.3556 7.19919 31.3556 7.31384V25.1131C31.3556 27.9582 33.6819 30.2724 36.5418 30.2724H40.3143L40.3138 31.9821C40.3138 34.3352 37.693 34.5893 36.5662 34.5893Z"></path><path fill="#8A05BE" fill-rule="evenodd" clip-rule="evenodd" d="M22.9092 9.56052C22.9092 3.63703 16.2255 0 11.4338 0C8.2473 0 6.26787 1.5435 6.26787 4.02846L6.26736 5.72752H4.8262C2.52583 5.72752 0 8.24935 0 10.5459V28.6856C0 29.5786 0.730227 30.3049 1.62803 30.3049H6.05764C6.94782 30.3049 7.67805 29.5806 7.68567 28.6871H7.68414L7.6877 28.581L7.68567 28.477C7.68313 26.589 7.68465 12.6122 7.68567 7.13819H11.4582C13.5361 7.13819 15.2266 8.81958 15.2266 10.8868V28.6856C15.2266 29.5786 15.9568 30.3049 16.8546 30.3049H21.2883C22.1861 30.3049 22.9163 29.5786 22.9163 28.6856C22.9174 20.7211 22.9133 15.6542 22.9108 12.5329L22.9092 9.56052ZM11.4582 5.72752H7.68567L7.68617 4.01785C7.68617 1.66472 10.307 1.41067 11.4338 1.41067C15.6334 1.41067 21.4909 4.51131 21.4909 9.56052L21.493 12.5339C21.4955 15.6552 21.4996 20.7216 21.498 28.6856C21.498 28.8013 21.4041 28.8947 21.2883 28.8947H16.8546C16.7394 28.8947 16.6449 28.8013 16.6449 28.6856V10.8868C16.6449 8.04176 14.3181 5.72752 11.4582 5.72752ZM6.26787 28.6876C6.26685 28.8023 6.1724 28.8947 6.05764 28.8947H1.62803C1.51225 28.8947 1.41831 28.8013 1.41831 28.6856V10.5459C1.41831 9.01756 3.29719 7.13819 4.8262 7.13819H6.26736C6.26634 18.3013 6.26533 28.5826 6.26787 28.6876Z"></path></svg></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="ativo item-nav">
+                        <a class="item-nav ativo" href="#">Início</a>
+                    </li>
+                    <li class="item-nav">
+                        <a class="item-nav" href="#">Conta digital</a>
+                    </li>
+                    <li class="item-nav">
+                        <a class="item-nav" href="#">Cartão de crédito</a>
+                    </li>
+                    <li class="item-nav">
+                        <a class="item-nav" href="#">Rewards</a>
+                    </li>
+                    <li class="item-nav">
+                        <a class="item-nav" href="#">Empréstimo</a>
+                    </li>
+                    <li class="item-nav">
+                        <a class="item-nav" href="#">Sobre nós</a>
+                    </li>
+                    <li class="item-nav">
+                        <a class="item-nav" href="#">Carreiras</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="ativo item-nav">
+                        <a class="ativo item-nav" href="#">Login</a>
+                    </li>
+                    <li class="nubank-button align-middle botao-cor">
+                        <a href="#">Quero ser Nubank</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </section>
+
+    <section id="primeiro" class="mt-5">
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col col-lg-6 col-md-6 col-sm-12">
+                        <span class="titulo">Ser Nubank é reinventar sua vida financeira</span><br><br>
+                        <span class="subtitulo">
+                            Conta com rendimento acima<br>
+                            da poupança, cartão de crédito<br>
+                            sem anuidade e o melhor:<br>
+                            resolva tudo pelo app.<br><br>
+                            <button class="botao-cor">Quero ser Nubank</button>
+                        </span>
+                    </div>
+                    <div class="col col-lg-6 col-md-6 col-sm-12">
+                        <img src="img/1-cel.png" class="img-fluid imagem">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="segundo">
+        <div class="jumbotron">
+            <div class="container-fluid">
+                <div class="row">
+                    <div id="esquerda" class="col col-lg-5 col-md-5 col-sm-12">
+                        <span class="titulo">Conta do Nubank</span><br>
+                        <span class="titulo cinza">Nossa conta digital<br>
+                                    com mais de 20<br>
+                                    milhões de clientes<br>
+                                    em todo o Brasil.
+                        </span><br><br>
+                        <a href="https://blog.nubank.com.br/seu-dinheiro-esta-seguro-na-conta-nubank/">Segura e sem complicações</a>
+                    </div>
+                    <div class="col col-lg-7 col-md-7 col-sm-12">
+                        <img src="img/2-cel.png" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="terceiro">
+        <div class="jumbotron pb-0">
+            <div class="container-fluid m-0 p-0">
+                <div class="row">
+                    <div id="esquerda" class="col col-lg-5 col-md-5 col-sm-12 pl-5">
+                        <span class="titulo">Cartão de Crédito</span><br>
+                        <span class="titulo cinza">Pode chamar ele de<br>
+                                                    roxinho. Além disso,<br>
+                                                    pode chamar ele de<br>
+                                                    moderno, gratuito e<br>
+                                                    prático também.
+                        </span><br><br>
+                        <a href="https://blog.nubank.com.br/seu-dinheiro-esta-seguro-na-conta-nubank/">Saiba mais</a><br><br><br>
+                    </div>
+                    <div class="col col-lg-7 col-md-7 col-sm-12">
+                        <img src="img/3.png" width="230px" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="quarto">
+        <div class="jumbotron">
+            <div class="container-fluid">
+                <div class="row">
+                    <div id="esquerda" class="col col-lg-5 col-md-5 col-sm-12">
+                        <span class="titulo text-white">Rewards</span><br>
+                        <span class="titulo cinza">Um programa de<br>
+                                                    pontos justo e fácil<br>
+                                                    de usar.
+                        </span><br><br>
+                        <a class="text-white" href="https://blog.nubank.com.br/seu-dinheiro-esta-seguro-na-conta-nubank/">Saiba mais</a>
+                    </div>
+                    <div class="col col-lg-7 col-md-7 col-sm-12" id="imagem">
+                        <img src="img/3-cel.png" width="450px" class="img-fluid float-right pr-5">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="quinto">
+        <div class="jumbotron">
+            <div class="container-fluid">
+                <div class="row">
+                    <div id="esquerda" class="col col-lg-5 col-md-5 col-sm-12">
+                        <span class="titulo text-white">Reinventando<br>
+                                                        a relação das<br>
+                                                        pessoas com<br>
+                                                        o dinheiro
+                        </span><br><br>
+                        <a class="text-white" href="https://blog.nubank.com.br/seu-dinheiro-esta-seguro-na-conta-nubank/">É nisso que acreditamos</a><br><br>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="sexto">
+        <div class="jumbotron">
+            <div class="container-fluid">
+                <div class="row">
+                    <div id="esquerda" class="col col-lg-5 col-md-5 col-sm-12">
+                        <span class="titulo text-white">NuCommunity<p></p>
+                                                    A comunidade<br>
+                                                    oficial do Nubank
+                        </span><br><br>
+                        <a class="text-white" href="https://blog.nubank.com.br/seu-dinheiro-esta-seguro-na-conta-nubank/">Tire dúvidas, compartilhe ideias e faça parte</a><br><br><br><br>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="setimo">
+        <div class="jumbotron">
+            <div class="container-fluid">
+                <div class="row">
+                    <div id="esquerda" class="col col-lg-5 col-md-5 col-sm-12">
+                        <span class="titulo text-white">Um blog para você<br>
+                                                            repensar o seu<br>
+                                                            dinheiro
+                        </span><br><br>
+                        <a class="text-white" href="https://blog.nubank.com.br/seu-dinheiro-esta-seguro-na-conta-nubank/">Conheça o Fala, Nubank</a><br><br><br><br>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="oitavo">
+        <div class="jumbotron">
+            <div class="container-fluid">
+                <div class="row">
+                    <div id="esquerda" class="col col-lg-5 col-md-5 col-sm-12">
+                        <div class="bloco">
+                            <span class="titulo">Nubank</span><br><br>
+                            <span class="texto">Sobre nós</span><br>
+                            <span class="texto">Carreiras</span><br>
+                            <span class="texto">Perguntas frequentes</span><br>
+                            <span class="texto">Contato</span><br>
+                            <span class="texto">Imprensa</span><br>
+                        </div>
+                        <div class="bloco">
+                            <span class="titulo">Explore </span><br><br>
+                            <span class="texto">Comunidade</span><br>
+                            <span class="texto">Blog</span><br>
+                            <span class="texto">Newsletter</span><br>
+                        </div>
+                        <div class="bloco">
+                            <span class="titulo">Transparência</span><br><br>
+                            <span class="texto">Política de Privacidade</span><br>
+                            <span class="texto">Política de Compliance</span><br>
+                            <span class="texto">Contratos</span><br>
+                            <span class="texto">Política de Segurança</span><br>
+                            <span class="texto">Demonstrações Financeiras</span><br>
+                            <span class="texto">Ética & Compliance</span><br>
+                            <span class="texto">Dados Abertos</span><br>
+                        </div>
+                        <div class="bloco">
+                            <span class="titulo">Fale com a gente</span><br><br>
+                            <span class="texto">0800 608 6236 </span><br>
+                            <span class="texto">meajuda@nubank.com.br </span><br>
+                            <span class="texto">Atendimento 24h</span><br>
+                        </div>
+                    </div>
+                    <div class="col col-lg-6 col-md-6 col-sm-12">
+                        <div class="bloco">
+                            <span class="titulo">Produtos</span><br><br>
+                            <span class="texto">Conta digital</span><br>
+                            <span class="texto">Cartão de crédito</span><br>
+                            <span class="texto">Rewards</span><br>
+                            <span class="texto">Empréstimo</span><br>
+                        </div>
+                        <div class="bloco">
+                            <span class="titulo">Pelo mundo</span><br><br>
+                            <span class="texto">México</span><br>
+                            <span class="texto">Argentina</span><br>
+                        </div>
+                        <div class="bloco">
+                            <span class="titulo">Ouvidoria</span><br><br>
+                            <span class="texto">0800 887 0463 </span><br>
+                            <span class="texto">ouvidoria@nubank.com.br</span><br>
+                            <span class="texto">Atendimento das 9h às 18h (dias úteis)</span><br>
+                        </div><br><br>
+                        <div>
+                            <span class="titulo">Baixe o app</span><br><br>
+                            <img src="img/apple.png" width="160px"><br><br>
+                            <img src="img/google.png" width="160px">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
